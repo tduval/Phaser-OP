50b79402e66114550cb07b3b82a77cfdb4c6c742
Thomas Duval <Thomas.Duval@emeriocorp.com>
2018-06-19 15:49:23 +0800

    move animation list to main.js file

--- assets/main.js
+++ assets/main.js
@@ -19,3 +19,26 @@ game.state.add("level01", level01);
 
 //Start the first State stage as Boot
 game.state.start("boot");
+
+var animPlayerList = ['idle_front',
+                    'run_side',
+                    'jump_front',
+                    'crouch_side',
+                    'atk_punch_side',
+                    'atk_kick_side',
+                    'atk_pistol_side',
+                    'atk_gatling_side',
+                    'run_atk_side',
+                    'atk_bazooka_side',
+                    'atk_stamp_side',
+                    'atk_head_side',
+                    'shout_win_side',
+                    'jump_win_side'
+                    ];
+
+var animEnemyList = ['run',
+                    'roll',
+                    'atk',
+                    'hit',
+                    'death',
+                    ];
