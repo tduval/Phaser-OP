dd28d62b7169d59f060fb3a7c861b233de8b8f68
tduval <duval.thomas01@gmail.com>
2018-06-20 13:48:59 +0800

    rename assets file name

--- assets/preload.js
+++ assets/preload.js
@@ -11,9 +11,9 @@ preload.prototype = {
 
         //load all the assets : images/sprites/spritesheet/atlas/tileset
         this.load.image('sky', './assets/images/sky.png'); // 112x304
-        this.load.image('clouds', './assets/images/clouds-1.png'); //800x421
-        this.load.image('sea', './assets/images/sea-1.png'); // 800x215
-        this.load.image('islands', './assets/images/island-1.png'); // 689x112
+        this.load.image('clouds', './assets/images/clouds.png'); //800x421
+        this.load.image('sea', './assets/images/sea.png'); // 800x215
+        this.load.image('islands', './assets/images/islands.png'); // 689x112
         this.load.image('far-grounds', './assets/images/far-grounds.png'); // 616x110
 
         this.load.image('tree', './assets/images/tree.png'); // 123x114
@@ -23,6 +23,7 @@ preload.prototype = {
         //load enemies npc
         this.load.atlas('npcCavermanSprite', './assets/images/npc/caverman.png', './assets/images/npc/npc.json');
         this.load.atlas('npcCaverman2Sprite', './assets/images/npc/caverman-2.png', './assets/images/npc/npc.json');
+
 
 	},
   	create: function(){
